3e0d64170b3a1ea422d2d3fc9c0de2cfa3d24ca6
Turupawn <turupawn@userr>
2014-09-08 07:10:52 -0600

    agenda

--- src/MiClase.java
+++ src/MiClase.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MiClase {
 
 	public static void main(String[] args)
-	{
+	{		
 		Scanner s = new Scanner(System.in);
 		
 		System.out.print("Ingrese el tamano de la agenda: ");
@@ -13,12 +13,14 @@ public class MiClase {
 		int telefonos[] = new int[tam]; 
 		
 		int opcion = 0;
-		while(opcion != 3)
+		do
 		{
 			System.out.println("Menu");
 			System.out.println("1: Ingresar un numero");
-			System.out.println("2: Leer un numero");
-			System.out.println("3: Salir");
+			System.out.println("2: Buscar por posicion");
+			System.out.println("3: Todos los numeros");
+			System.out.println("4: Buscar por nombre");
+			System.out.println("5: Salir");
 			
 			opcion=s.nextInt();
 			
@@ -43,11 +45,35 @@ public class MiClase {
 					pos2 = s.nextInt();
 					System.out.println(nombres[pos2]+" - "+telefonos[pos2]);
 					break;
+				case 3:
+					for(int i=0; i<nombres.length; i++)
+					{
+						System.out.println(nombres[i]+" - "+telefonos[i]);
+					}
+					break;
+				case 4:
+					String nombre_capturado;
+					System.out.print("Ingrese el nombre: ");
+					nombre_capturado=s.next();
+					for(int i=0; i< nombres.length; i++)
+					{
+						if(nombre_capturado.equals(nombres[i]))
+						{
+							System.out.println("Numero: "+telefonos[i]);
+						}
+					}
+					break;
+				case 5:
+					System.out.println("Adios");
+					break;
 				default:
 					System.out.println("Opcion invalida");
 					break;
 			}
-		}
+		}while(opcion != 5);
+		
+		
+		
 		
 //		for(int i = 0; i<nombres.length; i++)
 //		{
